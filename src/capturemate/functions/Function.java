c2adaefed06dd01463d313d1137e844efca45f9b
adrset <274433@pw.edu.pl>
2020-10-13 22:36:01 +0200

    test

--- src/capturemate/functions/Function.java
+++
@@ -1,5 +0,0 @@
-package capturemate.functions;
-
-public interface Function<T extends Number> {
-    public <T extends Number> double calculate(T arg1, T arg2 );
-}
--- src/capturemate/functions/Subtract.java
+++
@@ -1,9 +0,0 @@
-package capturemate.functions;
-
-public class Subtract <T extends Number> implements Function <T>{
-
-    @Override
-    public <T extends Number> double calculate(T arg1, T arg2) {
-        return arg1.doubleValue() - arg2.doubleValue();
-    }
-}
